2c7add2c820e328e4a5c8966353c56ff9a0ac212
Emmanuel Perotto <emmanuelperotto@gmail.com>
2018-05-29 17:43:24 -0300

    configure the redux in the index file

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-AppRegistry.registerComponent('courseproject', () => App);
+import React from "react";
+import App from "./App";
+import { AppRegistry } from "react-native";
+import { Provider } from "react-redux";
+import configureStore from "./src/store/store";
+
+const store = configureStore();
+
+const ReduxApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent("courseproject", () => ReduxApp);
