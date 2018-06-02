26a5abc7442648e62602f5a7f3900e375b8b5559
Emmanuel Perotto <emmanuelperotto@gmail.com>
2018-06-02 02:26:36 -0300

    add a listener to navigation

--- src/screens/SharePlaceScreen.js
+++ src/screens/SharePlaceScreen.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import PlaceInput from "../components/PlaceInput/PlaceInput";
 import { connect } from "react-redux";
 import { addPlace } from "../store/actions";
+import PropTypes from "prop-types";
 
 class SharePlaceScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
+  }
+
   render() {
     return (
       <View>
@@ -13,10 +19,29 @@ class SharePlaceScreen extends Component {
     );
   }
 
+  onNavigatorEvent = event => {
+    switch (event.type) {
+      case "NavBarButtonPress":
+        if (event.id === "sideDrawerToggle") {
+          this.props.navigator.toggleDrawer({
+            side: "left"
+          });
+        }
+        break;
+
+      default:
+        break;
+    }
+  };
+
   placeAddedHandler = placeName => {
     this.props.onAddPlace(placeName);
   };
 }
+
+SharePlaceScreen.propTypes = {
+  onAddPlace: PropTypes.func
+};
 
 const mapDispatchToProps = dispatch => {
   return {
