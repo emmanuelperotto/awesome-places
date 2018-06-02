0824fbbc6e25052b9e315f6d7f10915e5bfbec94
Emmanuel Perotto <emmanuelperotto@gmail.com>
2018-06-02 18:57:34 -0300

    add a default text input component

---
+++ src/components/UI/DefaultInput.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { TextInput, StyleSheet } from "react-native";
+import { LIGHT_GRAY } from "../../../colors";
+import PropTypes from "prop-types";
+
+const DefaultInput = props => (
+  <TextInput
+    underlineColorAndroid="transparent"
+    {...props}
+    style={[styles.input, props.style]}
+  />
+);
+
+const styles = StyleSheet.create({
+  input: {
+    borderWidth: 1,
+    borderColor: LIGHT_GRAY,
+    padding: 5,
+    margin: 8
+  }
+});
+
+DefaultInput.propTypes = {
+  style: PropTypes.object
+};
+
+export default DefaultInput;
