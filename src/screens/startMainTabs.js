0f0d1c83d920c61cd34db46a009ac4d7e8c204e5
Emmanuel Perotto <emmanuelperotto@gmail.com>
2018-06-02 00:32:54 -0300

    add buttons to side drawer

--- src/screens/startMainTabs.js
+++ src/screens/startMainTabs.js
@@ -4,7 +4,8 @@ import Icon from "react-native-vector-icons/Ionicons";
 const startTabs = () => {
   Promise.all([
     Icon.getImageSource("md-map", 30),
-    Icon.getImageSource("md-share-alt", 30)
+    Icon.getImageSource("md-share-alt", 30),
+    Icon.getImageSource("md-menu", 30)
   ]).then(sources => {
     Navigation.startTabBasedApp({
       tabs: [
@@ -12,15 +13,36 @@ const startTabs = () => {
           screen: "courseproject.FindPlaceScreen",
           label: "Find place",
           title: "Find place",
-          icon: sources[0]
+          icon: sources[0],
+          navigatorButtons: {
+            leftButtons: [
+              {
+                icon: sources[2],
+                title: "Menu"
+              }
+            ]
+          }
         },
         {
           screen: "courseproject.SharePlaceScreen",
           label: "Share place",
           title: "Share place",
-          icon: sources[1]
+          icon: sources[1],
+          navigatorButtons: {
+            leftButtons: [
+              {
+                icon: sources[2],
+                title: "Menu"
+              }
+            ]
+          }
         }
-      ]
+      ],
+      drawer: {
+        left: {
+          screen: "courseproject.SideDrawer"
+        }
+      }
     });
   });
 };
