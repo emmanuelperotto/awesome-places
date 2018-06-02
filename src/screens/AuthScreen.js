250b7a65ba7c5193771f66cc461737b22fee803d
Emmanuel Perotto <emmanuelperotto@gmail.com>
2018-06-02 18:57:53 -0300

    refactor auth screen

--- src/screens/AuthScreen.js
+++ src/screens/AuthScreen.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
-import { View, Button } from "react-native";
+import { Text, View, Button, StyleSheet } from "react-native";
 
 import startMainTabs from "./startMainTabs";
+import DefaultInput from "../components/UI/DefaultInput";
+import { LIGHT_GRAY, DARK_GRAY } from "../../colors";
 
 class AuthScreen extends Component {
   render() {
     return (
-      <View>
-        <Button title="Login" onPress={this.loginHandler} />
+      <View style={styles.container}>
+        <Text>Please log in</Text>
+        <Button title="Switch to Login" />
+
+        <View style={styles.inputContainer}>
+          <DefaultInput
+            placeholder="Your e-mail address"
+            style={styles.input}
+          />
+          <DefaultInput placeholder="Password" style={styles.input} />
+          <DefaultInput placeholder="Confirm Password" style={styles.input} />
+        </View>
+
+        <Button title="Submit" onPress={this.loginHandler} />
       </View>
     );
   }
@@ -16,5 +30,20 @@ class AuthScreen extends Component {
     startMainTabs();
   };
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  inputContainer: {
+    width: "80%"
+  },
+  input: {
+    backgroundColor: LIGHT_GRAY,
+    borderColor: DARK_GRAY
+  }
+});
 
 export default AuthScreen;
