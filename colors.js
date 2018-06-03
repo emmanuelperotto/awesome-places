13b9f57458d6e9ac44f58c48d37fe363b63f4f3f
Emmanuel Perotto <emmanuelperotto@gmail.com>
2018-06-03 00:43:23 -0300

    add more colors

--- colors.js
+++ colors.js
@@ -1,3 +1,6 @@
-export const RED = "red";
+export const RED = "#F00";
+export const BLACK = "#000";
+export const TRANSPARENT = "transparent";
 export const LIGHT_GRAY = "#eee";
 export const DARK_GRAY = "#bbb";
+export const LIGHT_BLUE = "#29aaf4";
